9c9e2bb3fa7ace5aec54c28805d678c022d232f6
Mik <mik.bry@gmail.com>
2020-05-27 16:59:49 +0200

    feat: common package created (#69)

---
+++ packages/common/src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@mikbry.com
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { friendlyDate } from './utils/dateTools';
+import deepCopy from './utils/deepCopy';
+
+export { friendlyDate, deepCopy };
--- packages/webchat/src/utils/builder.js
+++ packages/webchat/src/utils/builder.js
@@ -5,8 +5,8 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { deepCopy } from '@ziiircom/common';
 import { genClassRules } from './styled/genStyle';
-import deepCopy from './deepCopy';
 
 export const createElement = (element, props, ...children) => {
   let definition;
