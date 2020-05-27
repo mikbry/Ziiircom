13855ea4c1669986fff0558d437188308ebdeec5
Mik <mik@opla.ai>
2020-05-27 16:58:36 +0200

    feat: common package created

---
+++ packages/common/src/utils/dateTools.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@mikbry.com
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const MILLISECONDS_TO_SECONDS = 0.001;
+const SECONDS_IN_YEAR = 31557600;
+const SECONDS_IN_MONTH = 2629800;
+const SECONDS_IN_DAY = 86400;
+const SECONDS_IN_HOUR = 3600;
+const SECONDS_IN_MINUTE = 60;
+
+export const friendlyDate = (date, locale = 'en') => {
+  let remaining = (Date.now() - date) * MILLISECONDS_TO_SECONDS;
+  const years = Math.floor(remaining / SECONDS_IN_YEAR);
+  remaining -= years * SECONDS_IN_YEAR;
+  const months = Math.floor(remaining / SECONDS_IN_MONTH);
+  remaining -= months * SECONDS_IN_MONTH;
+  const days = Math.floor(remaining / SECONDS_IN_DAY);
+  remaining -= days * SECONDS_IN_DAY;
+  if (months || years || days > 6) {
+    return new Date(date).toLocaleString(locale);
+  }
+  if (days > 1) {
+    return new Date(date).toLocaleString(locale);
+  }
+  if (days === 1) {
+    return 'yesterday';
+  }
+  const hours = Math.floor(remaining / SECONDS_IN_HOUR);
+  remaining -= hours * SECONDS_IN_HOUR;
+  if (hours > 0) {
+    return `${hours} hours ago`;
+  }
+  const mins = Math.floor(remaining / SECONDS_IN_MINUTE);
+  remaining -= mins * SECONDS_IN_MINUTE;
+  if (mins > 0) {
+    return `${mins} mins ago`;
+  }
+  if (remaining > 10) {
+    return `${remaining} secs ago`;
+  }
+  return 'just now';
+};
--- packages/webchat/src/hooks/echo.js
+++ packages/webchat/src/hooks/echo.js
@@ -5,7 +5,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import deepCopy from '../utils/deepCopy';
+import { deepCopy } from '@ziiircom/common';
 
 // simple Echo Messaging service
 const useEcho = (listener = () => {}) => {
