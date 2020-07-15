c9bfd9893c8a5d0755006faef8b7556c75b5be8d
Mik <mik@miklabs.com>
2020-07-15 16:04:19 +0200

    feat: return entities

--- packages/dialog/src/dialog.js
+++ packages/dialog/src/dialog.js
@@ -53,13 +53,24 @@ const preprocessIntent = _intent => {
   return intent;
 };
 
-const renderTemplate = (template, context) => {
+const getValue = (_value, entities) => {
+  let value = _value;
+  if (value === '*') {
+    entities.forEach(e => {
+      if (e.type === 'any') {
+        ({ value } = e);
+      }
+    });
+  }
+  return value;
+};
+
+const renderTemplate = (template, context, entities) => {
   const set = {};
   const response = template.replace(/{{\s*([\w.]+)\s*(=\s*(.+)\s*)*}}/g, (match, _name, eq, replace) => {
     const name = _name.trim();
     if (eq) {
-      // TODO handle entities
-      set[name] = replace;
+      set[name] = getValue(replace, entities);
     }
     const value = set[name] || context[name] || '';
     return value;
@@ -86,8 +97,10 @@ const Dialog = (_intents, initialContexts) => {
       [match] = matchs;
     }
     let response;
+    let entities;
     if (match) {
-      const { intent, entities } = match;
+      const { intent } = match;
+      ({ entities } = match);
       // Store in context all variables changed
       if (intent.set) {
         context = { ...context, ...intent.set };
@@ -119,15 +132,16 @@ const Dialog = (_intents, initialContexts) => {
         context = { ...context, ...set };
       }
       if (output.set) {
-        // TODO handle entities
-        context = { ...context, ...output.set };
+        Object.keys(output.set).forEach(name => {
+          context[name] = getValue(output.set[name], entities);
+        });
       }
     } else {
       response = "I don't understand";
     }
     contexts[userId] = deepCopy(context);
     response = renderer(response);
-    return { response, context };
+    return { response, context, entities };
   };
   if (_intents && Array.isArray(_intents) && _intents.length) {
     const intents = _intents.map(i => preprocessIntent(i));
