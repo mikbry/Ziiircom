08fe9b56bcbc1601f25483d1fcb046551ecf946c
mikbry <mik@miklabs.com>
2020-08-12 12:40:58 +0200

    feat: webchat FAB animation

--- packages/messenger/src/MessengerApp.js
+++ packages/messenger/src/MessengerApp.js
@@ -18,12 +18,15 @@ const MessengerApp = async (messages, onMessage, onAction, hideDate) => {
   ]);
   let isOpen = _isOpen;
   const messengerRef = useRef(null);
+  const fabRef = useRef(null);
   const handleFabClick = () => {
     isOpen = !isOpen;
     const messenger = messengerRef.current;
     messenger.classList.toggle('isclosed');
     messenger.classList.toggle('isopen');
     messenger.firstChild.classList.remove('isexpanded');
+    const fab = fabRef.current;
+    fab.classList.toggle('isactive');
   };
 
   if (header.closeButton) {
@@ -79,14 +82,27 @@ const MessengerApp = async (messages, onMessage, onAction, hideDate) => {
     }
   `;
 
-  const Fab = styled(ui.FAButton, { className: 'ziiircom-messenger-fab', onClick: handleFabClick })`
+  const Fab = styled(ui.FAButton, {
+    ref: fabRef,
+    className: `ziiircom-messenger-fab ${isOpen ? 'isactive' : ''}`,
+    onClick: handleFabClick,
+    iconActive:
+      "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='-12 -12 48 48'%3E%3Cg fill='white'%3E%3Cpath d='M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z'/%3E%3C/g%3E%3C/svg%3E",
+  })`
     position: absolute;
     right: 32px;
     bottom: 32px;
     z-index: 9;
   `;
 
-  return styled('div', { className: 'ziiircom-messenger-frame' }, MessengerBox, Fab)``;
+  return styled(
+    'div',
+    {
+      className: 'ziiircom-messenger-frame',
+    },
+    MessengerBox,
+    Fab,
+  )``;
 };
 
 export default MessengerApp;
