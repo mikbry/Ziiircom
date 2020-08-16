9686294fa73e849bbe73f80455b30a8a7afbb48a
Mik <mik.bry@gmail.com>
2020-08-16 20:52:10 +0200

    chore: fix GA merge is not a function (#267)

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common');
 
