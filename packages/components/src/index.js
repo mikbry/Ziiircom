264a69d6ad7171fcd95bc86d3658dabf8236d6cb
Mik <mik.bry@gmail.com>
2020-08-17 11:00:26 +0200

    chore: components remove warning (#269)

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -9,9 +9,11 @@ import Interface, { setup } from './interface';
 
 export { setup, Interface };
 
-export default async function () {
+const ui = async () => {
   const Messenger = await import('./Messenger/index.js');
   const Message = await import('./components/Message/index.js');
   const FAButton = await import('./components/FAButton/index.js');
   return { Messenger: Messenger.default, Message: Message.default, FAButton: FAButton.default };
-}
+};
+
+export default ui;
