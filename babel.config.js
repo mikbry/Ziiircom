6a1494bee7d94ced782e66ccd6374320a33a1d8a
Mik <mik.bry@gmail.com>
2020-08-20 14:35:58 +0200

    chore: jest (#274)

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'commonjs',
+        debug: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [],
+};
